e9c0d124aca79af6701fcb3a7bf7629205f4b254
Navsea <kenneth.dl@gmail.com>
2017-11-05 19:25:33 +0100

    done by script

--- gpio_test.c
+++ gpio_test.c
@@ -13,8 +13,11 @@ int main(int argc, char **argv)
 	int i = 0;
 
 	gpio_initialize();
-	gpio_set_function(2, FSEL_OUTPUT);
-	gpio_set_function(3, FSEL_INPUT);
+	gpio_set_function(2, FSEL_OUTPUT);	// should toggle
+	gpio_set_function(3, FSEL_INPUT);	// should read whatever is connected
+	gpio_set_function(4, FSEL_INPUT);	// should detect rising edges
+
+	gpio_rising_edge_en(4, 1);			// enabling rising edge detection on pin 4
 
 	while(1)
 	{
@@ -28,9 +31,10 @@ int main(int argc, char **argv)
 			gpio_clear(2);
 		}
 
-		// this reads the output gpio2 and input gpio3
 		printf("gpio 2: %d\n", gpio_read(2));
 		printf("gpio 3: %d\n", gpio_read(3));
+		printf("gpio 4: %d\n", gpio_read(4));
+		printf("gpio 4 rising edge: %d\n", gpio_event(4));
 
 		i = !i;
 		sleep(2);
