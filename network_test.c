2591c667776173f83d5a932c87ed38d6ac31f1e7
Navsea <kenneth.dl@gmail.com>
2017-11-10 12:40:52 +0100

    done by script

---
+++ network_test.c
@@ -0,0 +1,13 @@
+/*
+ * network_test.c
+ *
+ *  Created on: 10 nov. 2017
+ *      Author: Kenneth
+ */
+
+#include "network.h"
+
+void main(int argc, char *argv[] )
+{
+	setup_client(argv[1], 80);
+}
