7439335270a7f860516f3e102261cc109a9b70d5
Navsea <kenneth.dl@gmail.com>
2017-11-12 18:21:53 +0100

    done by script

--- network_test_server.c
+++ network_test_server.c
@@ -16,7 +16,7 @@
 static int32_t client_socket = 0;
 static uint16_t client_send_timeout = CLIENT_TIMEOUT;
 
-static void alarm_handle(void );
+void alarm_handle(void );
 
 int main(int argc, char *argv[] )
 {
@@ -55,7 +55,7 @@ int main(int argc, char *argv[] )
 	return 0;
 }
 
-static void alarm_handle(void )
+void alarm_handle(void )
 {
 	printf("Timeout: didnt get request in time\n");
 	close(client_socket);
